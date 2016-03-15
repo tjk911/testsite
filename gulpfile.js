d01432f22ae2a3839a4a991724aa8d7c8bd37001
Kai Teoh <kai@ink-cafe.com>
2016-03-15 13:14:54 -0400

    Set templating engine, create json storage, begin json view

--- gulpfile.js
+++ gulpfile.js
@@ -22,21 +22,27 @@ gulp.task('default', ['sass'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
 });
 
-
-
-
 var express = require('express')
   , logger = require('morgan')
   , app = express()
   , port = process.env.PORT || 8000
   , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
-  , http = require('http')
-  , httpProxy = require('http-proxy')
+  // , http = require('http')
+  // , httpProxy = require('http-proxy')
   , bodyParser = require('body-parser')
+  , fs = require("fs")
+  , obj = require('./file.json')
+  // , stringify = JSON.stringify(obj)
+
+
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
+// Tell app to use jade
+app.set('view engine', 'jade');
+
+// Set up grabbing data properly with body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -47,20 +53,35 @@ app.get('/', function (req, res, next) {
   } catch (e) {
     next(e)
   }
+
+  console.log(obj);
+  console.log(stringify);
 })
 
 app.get('/fake', function (req, res, next) {
   try {
-    var html = template({ title: 'Fake' })
-    res.send(html)
+    // var html = template({ title: 'Fake' })
+    // res.send(html)
+    // var parsed = JSON.stringify(obj)
+    res.render('fake', { title : 'Fake', json: obj });
   } catch (e) {
     next(e)
   }
 })
 
 app.post('/', function(req, res){
-  console.log(JSON.stringify(req.body));
+  // var myJson = JSON.stringify(req.body);
+  obj.push(req.body);
+  var newJSON = JSON.stringify(obj);
+  fs.writeFile('file.json', newJSON, function (err) {
+    console.log(err);
+  });
+  // fs.appendFile( "file.json", JSON.stringify( myJson ), "utf8" );
 })
+
+
+// myJson = require("./file.json");
+
 
 app.listen(port, "localhost")
 
