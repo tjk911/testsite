8dd77458f092f1a2312c6139c1f95587dbd4f989
Kai Teoh <kai@ink-cafe.com>
2016-03-23 16:21:21 -0400

    Update readme (thanks to stackedit)

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 Run ``npm install`` to install dependencies, ``foundation watch`` to run the project, project code lives in Gulpfile.
 
-Using Jade for page templating.
+ - Using Jade for page templating.
+ - Proxy works on data server (through Apache2 proxypass config)
+ - Plugged in Angularjs
+ - Currently pushes data to both json file and a MongoDB
+ - Need to double check dependencies (specifically MongoDB)
+ - Need to work on proper routing/templating
+ - Schema and can, and should, be refactored into separate files
 
-Proxy works on data server (through Apache2 proxypass config) ~~but need to reconfigure node to recognize relative links for css/js~~ etc.
+Run ``forever gulpfile.js`` to keep service running on server "forever"
 
-Time to work on proper routing/templating
+Project currently being used for our RestaurantDB
--- gulpfile.js
+++ gulpfile.js
@@ -131,38 +131,6 @@ app.post('/', function(req, res){
   res.render('homepage', { title : 'Home' });
 })
 
-// var url = 'mongodb://localhost:27017/test';
-// MongoClient.connect(url, function(err, db) {
-//   assert.equal(null, err);
-//   console.log("Connected correctly to server.");
-//   db.close();
-// });
-
-
-
-
-// var deliciouseats = new Restaurant({
-//   name:"Restaurant North, Market North; Armonk, N.Y.",
-//   description:"One of the Hudson Valley's best farm-to-table restaurants. Owned by two alums from Danny Meyer's restaurant empire, Restaurant North serves the best local ingredients prepared by the talented James Beard Award-nominated chef Eric Gabrynowicz. The menu changes depending on what's in season, but always expect to see a cut of pork and beautiful tender greens. The white-tablecloth setting is serene and the service is flawless. For a more casual version, where you can get sandwiches and salads to go, head across the street to Market North, the little sister cafe.",
-//   detour:"Not five minutes off I-684. Exit 3N, Armonk.",
-//   map:"https://www.google.com/maps/place/Restaurant+North/@41.125251,-73.716185,17z/data=!3m1!4b1!4m2!3m1!1s0x89c2bc4360856a93:0xb671856d5be0e15f",
-//   address:"386 Main St",
-//   city:"Armonk",
-//   state:"NY",
-//   zip:"10504",
-//   phone:"9142738686",
-//   website:"http://www.restaurantnorth.com",
-//   lat:"41.125251",
-//   long:"-73.7161797"
-// });
-
-// deliciouseats.save(function (err, deliciouseats) {
-//   if (err) return console.error(err);
-//   deliciouseats.announce();
-// });
-
-// Restaurant.find({name:"Restaurant North, Market North; Armonk, N.Y."}, console.log('found it!'));
-
 // LISTEN!
 app.listen(port, "localhost")
 
